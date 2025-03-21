0323c8e4842d57b44adfe842922fa426e0ae8619
Lohit Ganapati Jogi <17lohitjogi@gmail.com>
2025-03-21 19:02:37 +0530

    Add files via upload

---
+++ Voice Translator.py
@@ -0,0 +1,23 @@
+# Voice Translator in Python
+import googletrans
+import speech_recognition
+import gtts
+import playsound
+input_lang="hi"
+output_lang="fr"
+
+recognizer=speech_recognition.Recognizer()
+with speech_recognition.Microphone() as source:
+    print("speak now")
+    voice=recognizer.listen(source)
+    text=recognizer.recognize_google(voice, language=input_lang)
+    print(text)
+
+translator=googletrans.Translator()
+translation=translator.translate(text, dest=output_lang)
+print(translation.text)
+
+conver_audio = gtts.gTTS(translation.text, lang=output_lang)
+conver_audio.save("hello.mp3")
+playsound.playsound("hello.mp3")
+
